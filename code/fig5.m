43b0613303a60a201caaa2b9f0d4cafdca5ac794
Aaron Adler <aadler@uw.edu>
2017-12-04 21:08:53 -0800

    Half of a working memory neuron

--- code/fig5.m
+++ code/fig5.m
@@ -37,6 +37,8 @@ ys(:, :, 1) = [
 	-68.3737, 0.9820, 0.0631, 0.1259, 0
 	-68.3737, 0.9820, 0.0631, 0.1259, 0 ]';
 
+ys_mem = zeros( 5, length( ts ) );
+
 tau_t = 100; % milliseconds
 tau_b = 5;   % milliseconds
 % RK45 it on out
@@ -48,6 +50,13 @@ for n = 1:(length( ts )-1)
 	k3 = TEI_neurons(ts(n) + dt/2, ys(:, :, n) + dt/2 * k2, ext(ts(n)), tau_t, tau_b);
 	k4 = TEI_neurons(ts(n) + dt, ys(:, :, n) + dt * k3, ext(ts(n)), tau_t, tau_b);
 	ys(:, :, n+1) = ys(:, :, n) + (dt / 6 .* (k1 + 2*k2 + 2*k3 + k4));
+	
+	k1_mem = memory_neuron(ts(n), ys_mem(:, n), ys(:, :, n), tau_t );
+	k2_mem = memory_neuron(ts(n) + dt/2, ys_mem( :, n ) + dt/2 * k1, ys(:, :, n), tau_t);
+	k3_mem = memory_neuron(ts(n) + dt/2, ys_mem( :, n ) + dt/2 * k2, ys(:, :, n), tau_t);
+	k4_mem = memory_neuron(ts(n) + dt, ys_mem( :, n ) + dt * k3, ys(:, :, n), tau_t);
+	ys_mem(:, :, n+1) = ys_mem(:, n) + (dt / 6 .* (k1_mem + 2*k2_mem + 2*k3_mem + k4_mem));
+
 end
 
 % Plot the tonic neuron:
@@ -67,5 +76,5 @@ ylabel( 'Inhibitory Stimulus' );
 
 % Plot the inhibitory neuron:
 subplot( 4, 1, 4 );
-plot( ts, spike_rate( squeeze( ys( 1, 3, : ) ), ts ) );
+plot( ts, squeeze( ys_mem( 1, : ) ) );
 ylabel( 'Spike rate' ); 
---
+++ code/memory_neuron.m
@@ -0,0 +1,28 @@
+function dydt = memory_neuron( t, y, ext_y, tau )
+	I_intrinsic = intrinsic_current( y(1), y(2), y(3), y(4) );
+
+	W = 1.882;
+	W_e = 1;
+	W_i = 4;
+	W_0 = 3.8;
+	
+	V_e = 0;
+	V_i = -70;
+		
+	alpha = 1; % to make the effects of synaptic saturation weak, pg 174
+	% tau_0 = 100;
+	% tau_burst = 5;
+	
+	% Sigmoid, as defined in the paper:
+	theta_s = -20; % mV, pg 174
+	sigma_s = 2; % mV, pg 174
+	
+	s = (alpha .* (1 - y(5)) .* 1./(1 + exp(-(y(1)-theta_s)./sigma_s)) - y(5)) ./ tau;
+	
+	g_E = W * s + W_0 * ext_y(1, 5) + W_e * ext_y(2, 5);
+	g_I = W_i * ext_y(3, 5);
+
+	dydt = I_intrinsic - [g_E * ( y(1) - V_e ) + g_I * ( y(1) - V_i ), 0, 0, 0];
+
+	dydt = [ dydt; s ];
+end
